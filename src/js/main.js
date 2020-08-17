75bc33526e0e8b357290e97e724b0833f75ec791
kakahuy113 <kakahuy113@gmail.com>
2020-08-17 13:37:33 +0700

    reponse bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,113 +1,109 @@
 import {
-    getSVGs,
-    Loading
+	getSVGs,
+	Loading
 } from "./utilities/util";
 import Tab from './libraries/Tab'
 // active scrollBar window
 const scrollBar = () => {
-    const heigh = $(window).height();
-    const isChange = window.innerWidth > 1024;
-    if (isChange == true) {
-        $(window).scroll(function() {
-            if (heigh > 137) {
-                $("header").addClass('sroll');
-            } else {
-                $("header").removeClass("sroll");
-            }
-        });
-    }
+	const heigh = $(window).height();
+	const isChange = window.innerWidth > 1024;
+	if (isChange == true) {
+		$(window).scroll(function() {
+			if (heigh > 137) {
+				$("header").addClass('sroll');
+			} else {
+				$("header").removeClass("sroll");
+			}
+		});
+	}
 
 }
 const navMobile = () => {
-    const btn = $(".navMobile");
-    const nav = $(".navBar");
-    if (btn) {
-        $(btn).click(function(e) {
-            e.preventDefault();
-            $(btn).toggleClass('active');
-            $(nav).toggleClass("active");
-        });
-    }
-    const contentsMobile = document.querySelectorAll('[data_content--mobile]');
-    const contentsPc = document.querySelectorAll('[data_content--pc]');
-    const isChange = window.innerWidth < 1025;
-    if (isChange == true) {
-        contentsMobile.forEach((item) => {
-            const valueContent = item.getAttribute('data_content--mobile');
-            item.innerHTML = valueContent;
-        })
-    } else {
-        contentsPc.forEach((item) => {
-            const valueContent = item.getAttribute('data_content--pc');
-            item.innerHTML = valueContent;
-        })
-    }
+	const btn = $(".navMobile");
+	const nav = $(".navBar");
+	if (btn) {
+		$(btn).click(function(e) {
+			e.preventDefault();
+			$(btn).toggleClass('active');
+			$(nav).toggleClass("active");
+		});
+	}
+	const contentsMobile = document.querySelectorAll('[data_content--mobile]');
+	const contentsPc = document.querySelectorAll('[data_content--pc]');
+	const isChange = window.innerWidth < 1025;
+	if (isChange == true) {
+		contentsMobile.forEach((item) => {
+			const valueContent = item.getAttribute('data_content--mobile');
+			item.innerHTML = valueContent;
+		})
+	} else {
+		contentsPc.forEach((item) => {
+			const valueContent = item.getAttribute('data_content--pc');
+			item.innerHTML = valueContent;
+		})
+	}
 }
 const changeHeightFooter = () => {
-        const isChange = window.innerWidth < 768;
-        const height = $(".footer__inner").height();
-        if (isChange == true) {
-            $(".footer__wrapper").css('height', height + 150);
-            $(window).resize(function() {
-                console.log(height);
-                $(".footer__wrapper").css('height', height + 150);
-            });
-        }
-    }
-    // swiper home page video
+		const isChange = window.innerWidth < 768;
+		const height = $(".footer__inner").height();
+		if (isChange == true) {
+			$(".footer__wrapper").css('height', height + 150);
+			$(window).resize(function() {
+				console.log(height);
+				$(".footer__wrapper").css('height', height + 150);
+			});
+		}
+	}
+	// swiper home page video
 const swiperForBanner = () => {
-    var swiper = new Swiper(".hot-news--banner .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
+	var swiper = new Swiper(".hot-news--banner .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
 };
 
 //Swiper Hot Event
 const swiperHotEvent = () => {
-    var swiper = new Swiper(".home__stuff .swiper-container", {
-        slidesPerView: 3,
-        slidesPerGroup: 1,
-        breakpoints: {
-            300: {
-                slidesPerView: 1,
-            },
-            600: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-        navigation: {
-            nextEl: ".home__stuff .swiper-button-next",
-            prevEl: ".home__stuff .swiper-button-prev",
-        },
-    });
+	var swiper = new Swiper(".home__stuff .swiper-container", {
+		slidesPerView: 3,
+		slidesPerGroup: 1,
+		breakpoints: {
+			300: {
+				slidesPerView: 1,
+			},
+			600: {
+				slidesPerView: 2,
+			},
+			1024: {
+				slidesPerView: 3,
+			},
+		},
+		navigation: {
+			nextEl: ".home__stuff .swiper-button-next",
+			prevEl: ".home__stuff .swiper-button-prev",
+		},
+	});
 };
 
 //Swiper New Member
 const swiperNewMember = () => {
 	var swiper = new Swiper(".home__new-member .swiper-container", {
 		slidesPerView: 3,
-		simulateTouch: false,
 		slidesPerGroup: 1,
 		breakpoints: {
 			300: {
 				slidesPerView: 1,
 			},
-			500: {
+			600: {
 				slidesPerView: 2,
-			},
-			768: {
-				slidesPerView: 3,
 			},
 			1024: {
 				slidesPerView: 3,
@@ -145,30 +141,30 @@ const swiperNewMember = () => {
 
 //swiper happy birthday
 const swiperHappyBirthday = () => {
-    var swiper = new Swiper(".home__happy-birthday .swiper-container", {
-        slidesPerView: 3,
-        slidesPerGroup: 1,
-        breakpoints: {
-            300: {
-                slidesPerView: 1,
-            },
-            600: {
-                slidesPerView: 2,
-                spaceBetween: 25,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 45,
-            },
-        },
-        autoplay: {
-            delay: 2000,
-        },
-        navigation: {
-            nextEl: ".home__stuff .swiper-button-next",
-            prevEl: ".home__stuff .swiper-button-prev",
-        },
-    });
+	var swiper = new Swiper(".home__happy-birthday .swiper-container", {
+		slidesPerView: 3,
+		slidesPerGroup: 1,
+		breakpoints: {
+			300: {
+				slidesPerView: 1,
+			},
+			600: {
+				slidesPerView: 2,
+				spaceBetween: 25,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 45,
+			},
+		},
+		autoplay: {
+			delay: 2000,
+		},
+		navigation: {
+			nextEl: ".home__stuff .swiper-button-next",
+			prevEl: ".home__stuff .swiper-button-prev",
+		},
+	});
 };
 
 //swiper member homepage
@@ -179,9 +175,6 @@ const swiperMemberHome = () => {
 		loop: true,
 		breakpoints: {
 			300: {
-				slidesPerView: 1,
-			},
-			600: {
 				slidesPerView: 3,
 			},
 			1024: {
@@ -199,54 +192,54 @@ const swiperMemberHome = () => {
 	});
 };
 const swiperNews = () => {
-    const swiper = new Swiper(".block__HREC.right .swiper-container", {
-        slidesPerView: 1,
-        slidesPerColumn: 4,
-        slidesPerColumnFill: 'row',
-        speed: 800,
-        navigation: {
-            nextEl: ".block__HREC.right .swiper-button-next",
-            prevEl: ".block__HREC.right .swiper-button-prev",
-        },
-    });
-    const swipery = new Swiper(".block__HREC.left .swiper-container", {
-        slidesPerView: 1,
-        slidesPerColumn: 4,
-        slidesPerColumnFill: 'row',
-        speed: 800,
-        navigation: {
-            nextEl: ".block__HREC.left .swiper-button-next",
-            prevEl: ".block__HREC.left .swiper-button-prev",
-        },
-    });
+	const swiper = new Swiper(".block__HREC.right .swiper-container", {
+		slidesPerView: 1,
+		slidesPerColumn: 4,
+		slidesPerColumnFill: 'row',
+		speed: 800,
+		navigation: {
+			nextEl: ".block__HREC.right .swiper-button-next",
+			prevEl: ".block__HREC.right .swiper-button-prev",
+		},
+	});
+	const swipery = new Swiper(".block__HREC.left .swiper-container", {
+		slidesPerView: 1,
+		slidesPerColumn: 4,
+		slidesPerColumnFill: 'row',
+		speed: 800,
+		navigation: {
+			nextEl: ".block__HREC.left .swiper-button-next",
+			prevEl: ".block__HREC.left .swiper-button-prev",
+		},
+	});
 };
 
 // MAIN BANNER WEBSITE
 const initMainBanner = () => {
-    const namePage = document.querySelector("#js-page-verify");
-    const mainBanner = document.querySelector(".MainSlider__Banners");
-    if (
-        (namePage.className == "index-page") |
-        (namePage.className == "introduce-page")
-    ) {
-        mainBanner.classList.add("isIndex");
-    }
-    return new Swiper(".MainSlider__Banners .swiper-container", {
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        speed: 1000,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".MainSlider__Banners .swiper-pagination",
-            type: "bullets",
-            clickable: true,
-        },
-    });
+	const namePage = document.querySelector("#js-page-verify");
+	const mainBanner = document.querySelector(".MainSlider__Banners");
+	if (
+		(namePage.className == "index-page") |
+		(namePage.className == "introduce-page")
+	) {
+		mainBanner.classList.add("isIndex");
+	}
+	return new Swiper(".MainSlider__Banners .swiper-container", {
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		speed: 1000,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: ".MainSlider__Banners .swiper-pagination",
+			type: "bullets",
+			clickable: true,
+		},
+	});
 };
 
 const swipeIntroducePurpose = () => {
@@ -254,6 +247,11 @@ const swipeIntroducePurpose = () => {
 		slidesPerView: 3,
 		spaceBetween: 60,
 		slidesPerGroup: 1,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
 		breakpoints: {
 			300: {
 				slidesPerView: 1,
@@ -271,99 +269,99 @@ const swipeIntroducePurpose = () => {
 	});
 };
 const sniperEvent = () => {
-    const swiper = new Swiper('.eventBottom__HREC .swiper-container', {
-        slidesPerView: 2,
-        slidesPerColumn: 4,
-        slidesPerColumnFill: 'row',
-        speed: 800,
-        breakpoints: {
-            300: {
-                slidesPerView: 1,
-            },
-            1024: {
-                slidesPerView: 2,
-                slidesPerColumn: 4,
-            },
-        },
-        navigation: {
-            nextEl: '.eventBottom__HREC .swiper-button-next',
-            prevEl: '.eventBottom__HREC .swiper-button-prev',
-        },
-    });
+	const swiper = new Swiper('.eventBottom__HREC .swiper-container', {
+		slidesPerView: 2,
+		slidesPerColumn: 4,
+		slidesPerColumnFill: 'row',
+		speed: 800,
+		breakpoints: {
+			300: {
+				slidesPerView: 1,
+			},
+			1024: {
+				slidesPerView: 2,
+				slidesPerColumn: 4,
+			},
+		},
+		navigation: {
+			nextEl: '.eventBottom__HREC .swiper-button-next',
+			prevEl: '.eventBottom__HREC .swiper-button-prev',
+		},
+	});
 }
 const swiperNewAdc = () => {
-    const swiperL = new Swiper(".nSlider__col.left .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
+	const swiperL = new Swiper(".nSlider__col.left .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
 };
 const swiperNewAdcR = () => {
-    const swiperR = new Swiper(".nSlider__col.right .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
-    const swiperT = new Swiper(".eventTop__slider .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
-    const swiperB = new Swiper(".eventBot__slider .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
-    const swiperD = new Swiper(".detail__Adc .swiper-container", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        simulateTouch: false,
-    });
+	const swiperR = new Swiper(".nSlider__col.right .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
+	const swiperT = new Swiper(".eventTop__slider .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
+	const swiperB = new Swiper(".eventBot__slider .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
+	const swiperD = new Swiper(".detail__Adc .swiper-container", {
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		simulateTouch: false,
+	});
 };
 const showInputSearch = () => {
-    const btn = document.querySelector(".navSearch__btn");
-    const navSearch__input = document.querySelector(".navSearch__input");
-    if (btn) {
-        btn.addEventListener("click", (e) => {
-            btn.classList.toggle("active");
-            navSearch__input.classList.toggle("show");
-        });
-    }
+	const btn = document.querySelector(".navSearch__btn");
+	const navSearch__input = document.querySelector(".navSearch__input");
+	if (btn) {
+		btn.addEventListener("click", (e) => {
+			btn.classList.toggle("active");
+			navSearch__input.classList.toggle("show");
+		});
+	}
 };
 
 export const swiperExecutiveCommittee = () => {
@@ -444,6 +442,10 @@ const renderInfo = () => {
 			item.addEventListener("click", () => {
 				const temp = item.querySelector("a");
 				$(".tab-bch .executive-committee-active").html(temp.innerHTML);
+				const tempheight =$(".executive-committee-active").height();
+				// $('html, body').animate({
+				// 	scrollTop: $(".executive-committee-active").offset().top - 200
+				// }, 2000);
 			});
 		});
 	document
@@ -455,39 +457,40 @@ const renderInfo = () => {
 				const temp = item.querySelector("a");
 				$(".tab-bkt .executive-committee-active").html(temp.innerHTML);
 			});
+			
 		});
 };
 
 const copyDataByAttr = () => {
-    const items__paste = document.querySelectorAll('[data-paste]');
-    const items__copy = document.querySelectorAll('[data-copy]');
+	const items__paste = document.querySelectorAll('[data-paste]');
+	const items__copy = document.querySelectorAll('[data-copy]');
 
-    items__paste.forEach((itemPaste) => {
-        items__copy.forEach((itemCopy) => {
-            var data = itemCopy.getAttribute('data-copy');
-            if (data != null && data == itemPaste.getAttribute('data-paste')) {
-                itemPaste.textContent = itemCopy.textContent;
-            }
-        });
-    });
+	items__paste.forEach((itemPaste) => {
+		items__copy.forEach((itemCopy) => {
+			var data = itemCopy.getAttribute('data-copy');
+			if (data != null && data == itemPaste.getAttribute('data-paste')) {
+				itemPaste.textContent = itemCopy.textContent;
+			}
+		});
+	});
 };
 // ACTIVE LANGGUAGE
 const activeLanguage = () => {
-    const htmlLang = document.querySelector('html').getAttribute('lang');
-    const items__language = document.querySelectorAll(
-        '.header__languages .languages__item'
-    );
-    items__language.forEach((item) => {
-        if (item.getAttribute('data-language') == htmlLang) {
-            item.classList.add('active');
-        }
-    });
+	const htmlLang = document.querySelector('html').getAttribute('lang');
+	const items__language = document.querySelectorAll(
+		'.header__languages .languages__item'
+	);
+	items__language.forEach((item) => {
+		if (item.getAttribute('data-language') == htmlLang) {
+			item.classList.add('active');
+		}
+	});
 };
 //fake swiper partner
 const fakeSwiperPartner = () => {
 	if (document.querySelector(".index-page")) {
 		// CODE YOUR JS HERE
-		const array = [
+		var array = [
 			"url(../assets/images/partner/logo-1.png)",
 			"url(../assets/images/partner/logo-2.png)",
 			"url(../assets/images/partner/logo-3.png)",
@@ -506,13 +509,16 @@ const fakeSwiperPartner = () => {
 			itemChange.setAttribute("style", `--data-bg:${imageChange}`);
 		};
 		// KHỞI TẠO LIST HÌNH
-		const init = setInterval(autoChange, 2000);
+		const init = setInterval(autoChange, 500);
 		images.forEach((item, index) => {
 			item.setAttribute("style", `--data-bg:${array[index]}`);
-			item.addEventListener("mouseenter", (e) => {});
+			item.addEventListener("mouseenter", (e) => {
+				array.splice(`${array[index]}` , 1)
+				console.log(array);
+			});
 			item.addEventListener("mouseleave", (e) => {
-				setInterval(autoChange, 2000);
-				console.log("leave");
+				console.log(item.getAttribute("style"));
+				const init = setInterval(autoChange, 500);
 			});
 		});
 	}
